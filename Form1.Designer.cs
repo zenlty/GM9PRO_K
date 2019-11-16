c97926da01963fe668d29f433504133f490dec37
zenlty <barismizrakli@outlook.com>
2019-11-16 21:51:48 +0300

    Detecter has been added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.form = new XanderUI.XUIFormDesign();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.tab = new System.Windows.Forms.TabControl();
+            this.tab1 = new System.Windows.Forms.TabPage();
+            this.tab2 = new System.Windows.Forms.TabPage();
             this.grp_detecter = new XanderUI.XUICustomGroupbox();
             this.detecterbanner = new XanderUI.XUISuperButton();
             this.form.WorkingArea.SuspendLayout();
             this.form.SuspendLayout();
+            this.tab.SuspendLayout();
             this.grp_detecter.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,12 +60,52 @@
             // form.WorkingArea
             // 
             this.form.WorkingArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.form.WorkingArea.Controls.Add(this.log);
+            this.form.WorkingArea.Controls.Add(this.tab);
             this.form.WorkingArea.Controls.Add(this.grp_detecter);
             this.form.WorkingArea.Dock = System.Windows.Forms.DockStyle.Fill;
             this.form.WorkingArea.Location = new System.Drawing.Point(0, 39);
             this.form.WorkingArea.Name = "WorkingArea";
             this.form.WorkingArea.Size = new System.Drawing.Size(517, 303);
             this.form.WorkingArea.TabIndex = 0;
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(336, 28);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(178, 228);
+            this.log.TabIndex = 2;
+            this.log.Text = "";
+            // 
+            // tab
+            // 
+            this.tab.Controls.Add(this.tab1);
+            this.tab.Controls.Add(this.tab2);
+            this.tab.Location = new System.Drawing.Point(3, 6);
+            this.tab.Name = "tab";
+            this.tab.SelectedIndex = 0;
+            this.tab.Size = new System.Drawing.Size(331, 254);
+            this.tab.TabIndex = 1;
+            // 
+            // tab1
+            // 
+            this.tab1.Location = new System.Drawing.Point(4, 22);
+            this.tab1.Name = "tab1";
+            this.tab1.Padding = new System.Windows.Forms.Padding(3);
+            this.tab1.Size = new System.Drawing.Size(323, 228);
+            this.tab1.TabIndex = 0;
+            this.tab1.Text = "Tools";
+            this.tab1.UseVisualStyleBackColor = true;
+            // 
+            // tab2
+            // 
+            this.tab2.Location = new System.Drawing.Point(4, 22);
+            this.tab2.Name = "tab2";
+            this.tab2.Padding = new System.Windows.Forms.Padding(3);
+            this.tab2.Size = new System.Drawing.Size(323, 228);
+            this.tab2.TabIndex = 1;
+            this.tab2.Text = "Root Tools";
+            this.tab2.UseVisualStyleBackColor = true;
             // 
             // grp_detecter
             // 
@@ -111,8 +156,10 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GM 9 PRO KIT";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.form.WorkingArea.ResumeLayout(false);
             this.form.ResumeLayout(false);
+            this.tab.ResumeLayout(false);
             this.grp_detecter.ResumeLayout(false);
             this.grp_detecter.PerformLayout();
             this.ResumeLayout(false);
@@ -124,6 +171,10 @@
         private XanderUI.XUIFormDesign form;
         private XanderUI.XUICustomGroupbox grp_detecter;
         private XanderUI.XUISuperButton detecterbanner;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.TabControl tab;
+        private System.Windows.Forms.TabPage tab1;
+        private System.Windows.Forms.TabPage tab2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,29 +6,52 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 using System.Diagnostics;
+using System.Net;
+using System.IO;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace GM9PRO_Kit
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
-            RunCommand(adb,"devices");
         }
-
-        public static string path = Application.StartupPath.ToString() + "\\libs",
+        public static string path = Application.StartupPath.ToString() + "\\libs\\",
             adb = path + "adb.exe",
-            fastboot = path + "fastboot.exe";
+            fastboot = path + "fastboot.exe",
+            devicename;
         public static bool isthereconnecteddevice = true;
 
-
-
-
-        public void RunCommand(string tool,string code)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            detecter();
+        }
+        public void RunCommand(string tool, string code)
+        {
+            if (isthereconnecteddevice == true)
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.FileName = tool;
+                startInfo.Arguments = code;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            else
+            {
+                MessageBox.Show("GM 9 Pro not detected","Error Code : 001",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        void detecter()
         {
             Process process = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
@@ -36,11 +59,29 @@ namespace GM9PRO_Kit
             startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
-            startInfo.FileName = tool;
-            startInfo.Arguments = code;
+            startInfo.FileName = adb;
+            startInfo.Arguments = " shell getprop ro.product.model";
             process.StartInfo = startInfo;
             process.Start();
+            detecterbanner.ButtonText = process.StandardOutput.ReadToEnd();
+            if (detecterbanner.ButtonText == "" || detecterbanner.ButtonText == null)
+            {
+                detecterbanner.ButtonText = "GM 9 PRO Not Detected";
+                isthereconnecteddevice = false;
+            }
+            else
+            {
+                isthereconnecteddevice = true;
+                detecterbanner.ButtonText = "General Mobile GM 9 PRO Detected !";
+            }
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x219)
+            {
+                detecter();
+            }
+            base.WndProc(ref m);
         }
     }
-
 }
